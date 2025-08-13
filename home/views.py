22f3302b2b0c09b90326e8f9ce1f6eae82880940
Siddhartha Ranjan Lenka <138793207+Siddhartha-1609@users.noreply.github.com>
2025-08-13 07:09:50 +0530

    30846

--- home/views.py
+++ home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.config import settings 
+from django.http import HttpResponse
 # Create your views here.
+
+def homepage(request):
+    return HttpResponse(f"Welcome to {settings.RESTAURANT_NAME}")
