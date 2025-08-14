988182f5bf8c84368bcd71cc3753fa269bf3a725
Siddhartha Ranjan Lenka <138793207+Siddhartha-1609@users.noreply.github.com>
2025-08-14 14:50:55 +0530

    31998

--- home/views.py
+++ home/views.py
@@ -5,14 +5,14 @@ from django.http import HttpResponse
 
 def homepage(request):
     context = {
-        "restaurant_name" : "CHINKI'S",
+        "restaurant_name" : settings.RESTAURANT_NAME,
         "phone_number" : settings.RESTAURANT_PHONE
     }
     return render(request,'home.html',context)
 
 def about(request):
     context = {
-        "restaurant_name" : "CHINKI'S",
+        "restaurant_name" : settings.RESTAURANT_NAME,
         "description" : (
             "Family-owned restaurant serving fresh, locally sourced food."
             "We focus on warm hospitality and seasonal menus"
@@ -20,3 +20,13 @@ def about(request):
         "image_url" : "https//:images.pexels.com/photos/11236793/pexels-photo-11236793.jpg",
     }
     return render(request, 'about.html' , context)
+
+def contacr(request):
+    context = {
+        "restaurant_name" = settings.RESTAURANT_NAME,
+        "restaurant_phone" = settings.RESTAURANT_PHONE,
+        "restaurant_email" = settings.RESTAURANT_EMAIL,
+        "restaurant_address" = settings.RESTAURANT_ADDRESS,
+    }
+    return render(request, "contact.html", context)
+    
