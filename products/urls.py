d023a5939b9efe8e9cb23739560750d53667e752
Siddhartha Ranjan Lenka <138793207+Siddhartha-1609@users.noreply.github.com>
2025-08-15 15:37:11 +0530

    32950

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
-
+from .views import MenuView
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
-    path('menu/', views.menu , name="menu"),
+    path('api/menu/', MenuView.as_view() , name="menu-api"),
 ]
