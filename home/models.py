2ab6120a69f6af81a8c67aa25cf6b47ab5de1dc5
Siddhartha Ranjan Lenka <138793207+Siddhartha-1609@users.noreply.github.com>
2025-08-15 16:07:57 +0530

    32943

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class FeedBack(models.Model):
+    name = models.CharField(max_lenght = 100)
+    email = models.EmailField()
+    comments = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return f"Feedback from {self.name} ({self.email})"
