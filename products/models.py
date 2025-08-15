cf7d756d473506acad34c51d1dff8a349ae586bd
Siddhartha Ranjan Lenka <138793207+Siddhartha-1609@users.noreply.github.com>
2025-08-15 15:07:05 +0530

    32948

--- products/models.py
+++ products/models.py
@@ -13,7 +13,6 @@ class Menu(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank = True)
     price = models.DecimalField(max_digits=6,decimal_places=2)
-    is_available = models.BooleanField(default = True)
 
     def __str__(self):
         return self.name
