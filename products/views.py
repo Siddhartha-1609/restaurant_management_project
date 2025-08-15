ec92480e331648e13ea34f4ab2fef8f693bd40a3
Siddhartha Ranjan Lenka <138793207+Siddhartha-1609@users.noreply.github.com>
2025-08-15 15:41:31 +0530

    32950

--- products/views.py
+++ products/views.py
@@ -9,6 +9,8 @@ from .serializers import ItemSerializer
 '''
 NOTE: Conside this as a reference and follow this same coding structure or format to work on you tasks
 '''
+#for now hardcoded one with simple django temp without API
+
 try:
     from products.models import Menu
 except ImportError:
