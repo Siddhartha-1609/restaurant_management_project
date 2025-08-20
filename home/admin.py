530d1c037c2db85d39d8ba368cc346df33d0987a
Siddhartha Ranjan Lenka <138793207+Siddhartha-1609@users.noreply.github.com>
2025-08-20 17:39:33 +0530

    35997

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from .models import RestaurantLocation
 
 # Register your models here.
 admin.site.register(Feedback)
+
+class RestaurantLocationAdmin(admin.ModelAdmin):
+    list_display = ("address","city","state","zip_code")
