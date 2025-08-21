67925725861d1cf5b38ef2870075743e50c149ac
Siddhartha Ranjan Lenka <138793207+Siddhartha-1609@users.noreply.github.com>
2025-08-21 22:38:58 +0530

    36528

--- home/models.py
+++ home/models.py
@@ -13,7 +13,8 @@ class Feedback(models.Model):
 class Contact(models.Model):
     name = models.CharField(max_length = 100)
     email = models.EmailField()
-    created_at = models.DateTimeField(auto_now_add = True)
+    message = models.TextField()
+    submitted_at = models.DateTimeField(auto_now_add = True)
 
     def __str__(self):
         return self.name
