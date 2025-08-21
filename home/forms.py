a74fcc9fad63b486c024b7496cf31a656272ac21
Siddhartha Ranjan Lenka <138793207+Siddhartha-1609@users.noreply.github.com>
2025-08-21 22:39:18 +0530

    36528

--- home/forms.py
+++ home/forms.py
@@ -9,4 +9,4 @@ class FeedbackForm(forms.ModelForm):
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
-        fields = ["name" ,'email']
+        fields = ["name" ,'email','message']
