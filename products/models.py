43faad5b4f509668b3bd615d8fd8a35b4fab3abb
Siddhartha Ranjan Lenka <138793207+Siddhartha-1609@users.noreply.github.com>
2025-08-22 19:55:49 +0530

    37386

--- products/models.py
+++ products/models.py
@@ -19,14 +19,3 @@ class Menu(models.Model):
 
     def __str__(self):
         return self.name
-
-class Restaurant(models.Model):
-    name = models.CharField(max_length=255)
-    address = models.TextField()
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    zip_code = models.CharField(max_length=20)
-    phone_number = models.CharField(max_length = 20,blank=True,null=True)
-
-    def __str__(self):
-        return self.name
