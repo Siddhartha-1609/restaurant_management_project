19e027579c50e8bdcef615f4c93cf62b951aac93
Siddhartha Ranjan Lenka <138793207+Siddhartha-1609@users.noreply.github.com>
2025-08-27 09:59:27 +0530

    39128

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',views.homepage, name = 'homepage'),
@@ -8,4 +9,6 @@ urlpatterns = [
     path('reservation/',views.reservation, name = "reservation"),
     path('feedback/',views.feedback_view, name = "feedback"),
     path('faqs/',views.faqs , name= "FAQ's"),
+    path("login/",auth_views.LoginView.as_view(template_name="home.html"),name="login"),
+    path("logout/",auth_views.LogoutView.as_view(next_page="homepage"),name="logout"),
 ]
