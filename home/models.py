78ac88294e8eac62314aa5a0980dcbb65ec07d51
Siddhartha Ranjan Lenka <138793207+Siddhartha-1609@users.noreply.github.com>
2025-08-28 19:36:37 +0530

    39690

--- home/models.py
+++ home/models.py
@@ -37,6 +37,7 @@ class Restaurant(models.Model):
     zip_code = models.CharField(max_length=20)
     phone_number = models.CharField(max_length=20,blank=True,null=True)
     logo = models.ImageField(upload_to ="restaurant_logos/",blank=True,null=True)
+    
 
     def __str__(self):
         return self.name
