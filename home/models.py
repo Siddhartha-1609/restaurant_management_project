925aa6ba91f912ea6ec806cac576fac8f340f759
Siddhartha Ranjan Lenka <138793207+Siddhartha-1609@users.noreply.github.com>
2025-08-28 11:58:52 +0530

    39688

--- home/models.py
+++ home/models.py
@@ -40,3 +40,11 @@ class Restaurant(models.Model):
     def __str__(self):
         return self.name
 
+class Special(models.Model):
+    item_name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.item_name
